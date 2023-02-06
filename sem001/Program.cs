5e4f139c9de0335e7968cf30ea2255558d3850ca
RnDFoX <rnd-fox@mail.ru>
2023-02-06 02:44:01 +0200

    1 zadacha na seminare

---
+++ sem001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+
+System.Console.WriteLine("Vvedite pervoe chislo");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Vvedite vtoroe chislo");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b * b == a)
+{
+   Console.WriteLine("Pervoe chislo yavyaetsa kvadratom vtorogo");
+}
+else
+{
+    Console.WriteLine("нет");
+}
