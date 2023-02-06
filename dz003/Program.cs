03ba6a170eaa78995728e3faf9ad2503b5716dd2
RnDFoX <rnd-fox@mail.ru>
2023-02-06 04:59:15 +0200

    3 zadacha

---
+++ dz003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.WriteLine("Vvedite  chislo");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = (N % 2);
+if (a == 0)
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("нет");
+}
