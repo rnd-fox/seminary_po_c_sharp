0dd299df617b6a393052c3419cd727dde5f83e36
RnDFoX <rnd-fox@mail.ru>
2023-02-06 02:45:27 +0200

    2 zadacha na seminare

---
+++ sem002/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+Console.WriteLine("Введите номер дня недели");
+string den = Console.ReadLine();
+
+if(den == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+if(den == "2")
+{
+    Console.WriteLine("Вторник");
+}
+if(den == "3")
+{
+    Console.WriteLine("Среда");
+}
+if(den == "4")
+{
+    Console.WriteLine("Четверг");
+}
+if(den == "5")
+{
+    Console.WriteLine("Пятница");
+}
+if(den == "6")
+{
+    Console.WriteLine("Суббота");
+}
+if(den == "7")
+{
+    Console.WriteLine("Воскресенье");
+}
