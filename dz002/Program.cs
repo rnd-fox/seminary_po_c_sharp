5d72b9e19c17584f3b4a484295b2ee8b06844271
RnDFoX <rnd-fox@mail.ru>
2023-02-06 04:20:15 +0200

    2 zadacha

---
+++ dz002/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+System.Console.WriteLine("Vvedite pervoe chislo");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Vvedite vtoroe chislo");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Vvedite tret'e chislo");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.Write("max = ");
+        System.Console.Write(max = a);
+    }
+    else
+    {
+        Console.Write("max = ");
+        System.Console.Write(max = c);
+    }
+}
+else if (b > c)
+{
+    Console.Write("max = ");
+    System.Console.Write(max = b);
+}
+else
+{
+    Console.Write("max = ");
+    System.Console.Write(max = c);
+}
